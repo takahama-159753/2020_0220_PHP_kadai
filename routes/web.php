e3495b1a1d96147d4f5c3cfa3e9dd396026b7f8b
”高浜 <takahama.sin.04130413@gmail.com>
2020-02-23 10:37:39 +0000

    kadai_12_kari_t2

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,15 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
